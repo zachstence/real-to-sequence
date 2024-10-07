fe68d3802b95dd9b06ee4c9a4393d83629073a67
Zachary Stence <zachstence@gmail.com>
2024-10-06 20:01:02 -0500

    feat: realToSequence implementation

--- src/lib/real-to-sequence.ts
+++ src/lib/real-to-sequence.ts
@@ -1,3 +1,13 @@
 export const realToSequence = (n: number): number[] => {
-	return [1, 2, 3, 4];
+	if (n > 1) throw new Error('Enter a number between 0 and 1');
+
+	const binary = n.toString(2).split('.')[1].split('');
+	const sequence = binary.reduce<number[]>((sequence, value, index) => {
+		if (value === '1') {
+			sequence.push(index + 1);
+		}
+		return sequence;
+	}, []);
+
+	return sequence;
 };
